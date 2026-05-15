8fe03c187bb993a50b0c0c73bdabe5e2eab27791
nicolasspring <springn.ch@gmail.com>
2026-05-15 14:50:52 +0200

    dummy endpoints for reading posts

--- backend/api/api.py
+++ backend/api/api.py
@@ -3,6 +3,14 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.get("/posts")
+def get_posts():
+    return [
+        {
+            "id": "1",
+            "caption": "Weekend in Zurich",
+            "latitude": 47.3769,
+            "longitude": 8.5417,
+            "image_url": "...",
+        }
+    ]
