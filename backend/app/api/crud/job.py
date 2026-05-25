cdf42e68a4700ab048cc4f3815e5e5dc03afde11
nicolasspring <springn.ch@gmail.com>
2026-05-25 11:40:17 +0200

    add basic crud repos for posts and jobs

---
+++ backend/app/api/crud/job.py
@@ -0,0 +1,41 @@
+import typing as t
+from uuid import UUID
+
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.api.crud.base import BaseRepository
+from app.api.exceptions import ImportJobNotFoundException
+from app.models.job import ImportJob, ImportJobCreate, ImportJobUpdate
+
+
+class ImportJobRepository(BaseRepository):
+    model = ImportJob
+    exception_factory: t.Callable[[str, UUID], Exception] = (
+        lambda x, y: ImportJobNotFoundException(f"{x} with ID {y} not found.")
+    )
+
+    @classmethod
+    async def create(
+        cls,
+        session: AsyncSession,
+        item: ImportJobCreate,
+        exclude: t.Optional[list[str]] = None,
+        additional: t.Optional[dict[str, t.Any]] = None,
+    ) -> ImportJob:
+        return await super().create(session, item, exclude, additional)
+
+    @classmethod
+    async def read(cls, session: AsyncSession, id: UUID) -> ImportJob:
+        return await super().read(session, id)
+
+    @classmethod
+    async def read_all(cls, session: AsyncSession, **filters) -> list[ImportJob]:
+        return await super().read_all(session, **filters)
+
+    @classmethod
+    async def update(cls, session: AsyncSession, item: ImportJobUpdate) -> ImportJob:
+        return await super().update(session, item)
+
+    @classmethod
+    async def delete(cls, session: AsyncSession, id: UUID) -> ImportJob:
+        return await super().delete(session, id)
