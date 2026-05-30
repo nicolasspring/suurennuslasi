36fc0c3d3e84237d73d2400c5eab40647f15b122
nicolasspring <springn.ch@gmail.com>
2026-05-30 17:40:20 +0200

    use queue name variable

--- backend/packages/messaging/suurennuslasi_messaging/publisher/publisher.py
+++ backend/packages/messaging/suurennuslasi_messaging/publisher/publisher.py
@@ -5,12 +5,12 @@ import aio_pika
 from suurennuslasi_messaging.session import get_connection
 
 
-async def publish(payload: dict):
+async def publish(queue_name: str, payload: dict):
     connection = await get_connection()
     async with connection:
         channel = await connection.channel()
         queue = await channel.declare_queue(
-            "test",
+            queue_name,
             durable=True,
         )
         await channel.default_exchange.publish(
