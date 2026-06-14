3ef0421b15f0c1aa774de1d4af38b71db1250221
nicolasspring <springn.ch@gmail.com>
2026-06-14 19:13:10 +0200

    publish event and update job

--- backend/services/api/suurennuslasi_api/routes/ingestion.py
+++ backend/services/api/suurennuslasi_api/routes/ingestion.py
@@ -5,8 +5,10 @@ from fastapi import APIRouter, Depends, UploadFile
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from suurennuslasi_db.crud.job import ImportJobRepository
-from suurennuslasi_db.models.job import ImportJob, ImportJobCreate
+from suurennuslasi_db.models.job import ImportJob, ImportJobCreate, ImportJobUpdate
 from suurennuslasi_db.session.db import get_session
+from suurennuslasi_domain.constants import IMPORT_JOB_STATUS
+from suurennuslasi_messaging.publisher import publish
 from suurennuslasi_storage.crud.storage import AsyncObjectStorage
 
 router = APIRouter(tags=["ingestion"])
@@ -23,8 +25,10 @@ async def ingest_file(
     with tempfile.NamedTemporaryFile() as tmp:
         await AsyncObjectStorage.download_to_path(object_key, tmp.name)
     await AsyncObjectStorage.delete(object_key)
-    # publish event
-    # change job status to queued
+    await publish("import.created", {"job_id": job.id, "object_key": object_key})
+    await ImportJobRepository.update(
+        session, ImportJobUpdate(id=job.id, status=IMPORT_JOB_STATUS.QUEUED)
+    )
     return job
 
 
