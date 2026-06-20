ee48f75845348337eb1534cfcc1809810de6007b
nicolasspring <springn.ch@gmail.com>
2026-06-20 09:11:10 +0200

    explicitly log retries when connecting to rabbitmq

--- backend/packages/messaging/suurennuslasi_messaging/session/session.py
+++ backend/packages/messaging/suurennuslasi_messaging/session/session.py
@@ -7,9 +7,12 @@ import aio_pika
 async def get_connection():
     while True:
         try:
-            return await aio_pika.connect_robust(
+            print("Connecting to RabbitMQ...")
+            conn = await aio_pika.connect_robust(
                 f"amqp://{os.getenv('RABBITMQ_USER')}:{os.getenv('RABBITMQ_PASSWORD')}@rabbitmq/"
             )
+            print("Connected to RabbitMQ")
+            return conn
         except Exception as exc:
             print(f"RabbitMQ not ready: {exc}")
             await asyncio.sleep(5)
