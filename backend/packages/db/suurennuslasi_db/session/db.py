978c9a8114950a70816b64656739ba824a4c7644
nicolasspring <springn.ch@gmail.com>
2026-07-26 11:15:10 +0200

    make db env var project-specific

--- backend/packages/db/suurennuslasi_db/session/db.py
+++ backend/packages/db/suurennuslasi_db/session/db.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_asyn
 from suurennuslasi_db.session.config import settings
 
 engine = create_async_engine(
-    settings.DATABASE_URL,
+    settings.SUURENNUSLASI_DATABASE_URL,
     echo=True,
 )
 
