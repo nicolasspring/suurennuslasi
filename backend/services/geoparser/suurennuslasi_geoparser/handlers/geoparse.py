3ef2f609530396d89b0241e9f8df7b07aea3cf26
nicolasspring <springn.ch@gmail.com>
2026-07-31 14:14:25 +0200

    update post with location

--- backend/services/geoparser/suurennuslasi_geoparser/handlers/geoparse.py
+++ backend/services/geoparser/suurennuslasi_geoparser/handlers/geoparse.py
@@ -7,6 +7,7 @@ from geoparser.modules import SpacyRecognizer
 from suurennuslasi_db.crud.job import ImportJobRepository
 from suurennuslasi_db.crud.post import PostRepository
 from suurennuslasi_db.models.job import ImportJobUpdate
+from suurennuslasi_db.models.post import PostUpdate
 from suurennuslasi_db.session.db import AsyncSessionLocal
 from suurennuslasi_domain.constants import IMPORT_JOB_STATUS
 from suurennuslasi_events.events.models import PostParsed
@@ -29,7 +30,26 @@ async def geoparse_post(event: PostParsed):
         logger.info(f"Geoparsing post {event.post_id} for job {event.job_id}")
         post = await PostRepository.read(session, event.post_id)
         document = geoparser.parse(post.caption)[0]
-        for toponym in document.toponyms:
-            logger.info(
-                f"Found toponym {toponym.text} in post {event.post_id} for job {event.job_id}"
-            )
+        if document.toponyms:
+            # for now, the first toponym found serves as the post location
+            first_toponym = document.toponyms[0]
+            if post_location := first_toponym.location:
+                location = post_location.data.get("name")
+                latitude = post_location.data.get("latitude")
+                longitude = post_location.data.get("longitude")
+                await PostRepository.update(
+                    session,
+                    PostUpdate(
+                        id=post.id,
+                        location=location,
+                        latitude=latitude,
+                        longitude=longitude,
+                    ),
+                )
+                logger.info(f"Updated post {event.post_id} with location {location}")
+            else:
+                logger.info(
+                    f"Toponym {first_toponym.text} for post {event.post_id} has no location"
+                )
+        else:
+            logger.info(f"No toponyms detected for post {event.post_id}")
