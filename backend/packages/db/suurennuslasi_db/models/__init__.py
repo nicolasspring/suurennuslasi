da8a3916eef46ba99fa643961bff7484fa289d73
nicolasspring <springn.ch@gmail.com>
2026-08-02 09:31:51 +0200

    rebuild all models

--- backend/packages/db/suurennuslasi_db/models/__init__.py
+++ backend/packages/db/suurennuslasi_db/models/__init__.py
@@ -1,7 +1,24 @@
-from .job import ImportJob
-from .source import Source
-from .post import Post
-from .media import Media
+from .job import ImportJob, ImportJobCreate, ImportJobRead, ImportJobUpdate
+from .media import Media, MediaCreate, MediaRead, MediaUpdate
+from .post import Post, PostCreate, PostRead, PostUpdate
+from .source import Source, SourceCreate, SourceRead, SourceUpdate
 
-for model in (Source, Post, Media):
+for model in (
+    ImportJob,
+    ImportJobCreate,
+    ImportJobRead,
+    ImportJobUpdate,
+    Media,
+    MediaCreate,
+    MediaRead,
+    MediaUpdate,
+    Post,
+    PostCreate,
+    PostRead,
+    PostUpdate,
+    Source,
+    SourceCreate,
+    SourceRead,
+    SourceUpdate,
+):
     model.model_rebuild()
